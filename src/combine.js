75cb71f3928a0d164f7a42a3eaa793779ff129d1
Michael Klein <michael.klein.business@gmail.com>
2019-05-05 16:28:43 +0200

    another minor fix

--- src/combine.js
+++ src/combine.js
@@ -2,8 +2,12 @@ import { stream } from "./stream";
 export const combine = (...streams$) => {
   const values = Array(streams$.length).fill(undefined);
   const newStream$ = stream();
+
   streams$.forEach((stream$, index) => {
     values[index] = stream$();
+  });
+  newStream$(values);
+  streams$.forEach((stream$, index) => {
     stream$.on(value => {
       values[index] = value;
       newStream$(values);
